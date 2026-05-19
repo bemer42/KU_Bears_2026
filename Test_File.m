4307a7b37cbc4a07bcb259bbca69f3c22d8b2108
bemer42 <39095252+bemer42@users.noreply.github.com>
2026-05-19 14:36:15 -0400

    small updates

--- Test_File.m
+++ Test_File.m
@@ -1,1 +1,23 @@
-% Hi this is Brooks
+% This  is a test. 
+clear all
+close all
+clc 
+
+% Time Discretization: 
+N     = 1e4;
+t_0   = 0; 
+t_end = 10;
+t     = linspace(t_0, t_end, N);
+
+%  Define a function: 
+f = @(t) 1/30 * exp(-t.^2) .* cos(t);
+
+% Plot: 
+figure(1)
+plot(t,f(t),'k','linewidth',5)
+title('My First Plot','fontsize',20)
+grid on
+grid minor
+
+
+
